eb86b1e5c15aba58d538af0befeee72f120ecf6d
Hamza-Chaiboub <hamzachb93@gmail.com>
2026-01-30 10:16:29 -0500

    load env vars && air for hot reload in dev

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -4,14 +4,55 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 )
 
+type Config struct {
+	AppEnv      string
+	Port        string
+	CorsOrigin  string
+	DatabaseURL string
+	RedisURL    string
+}
+
 func main() {
+	config := loadConfig()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode("Loaded")
+		json.NewEncoder(w).Encode(config)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", mux))
+}
+
+func loadConfig() Config {
+	appEnv := getEnv("APP_ENV", "development")
+	port := getEnv("BACKEND_PORT", "8080")
+	allCors := getEnv("CORS_ORIGIN", "http://localhost:5173")
+	databaseURL := getEnv("DATABASE_URL", "")
+	redisURL := getEnv("REDIS_URL", "")
+
+	if databaseURL == "" {
+		log.Fatal("DATABASE_URL not found")
+	}
+	if redisURL == "" {
+		log.Fatal("REDIS_URL not found")
+	}
+
+	return Config{
+		AppEnv:      appEnv,
+		Port:        port,
+		CorsOrigin:  allCors,
+		DatabaseURL: databaseURL,
+		RedisURL:    redisURL,
+	}
+}
+
+func getEnv(key string, fallback string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return fallback
+	}
+	return value
 }
