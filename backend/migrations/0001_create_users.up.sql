30651d911b154c668ec52b9c7fb73232c3e477bb
Hamza-Chaiboub <hamzachb93@gmail.com>
2026-01-31 19:45:31 -0500

    migration service and first users migration

---
+++ backend/migrations/0001_create_users.up.sql
@@ -0,0 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email text NOT NULL UNIQUE,
+    password_hash text NOT NULL,
+    created_at timestamptz NOT NULL DEFAULT now()
+);
